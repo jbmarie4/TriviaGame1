edf1b37b340cd0b4f972026f0c88f1f7322a71a3
Jamie Marie Burgess <jbmariebg4@gmail.com>
2018-03-01 01:48:43 -0600

    updates to styling, results button function, html content

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # TriviaGame1
+
+Eugenics Trivia Game: Learning More About a Dark Time in Our History
+
+You have 60 seconds to try to answer all of the trivia questions. When time runs out, a message will tell you how many you answered correctly. 
+
+You can also click the results button to see how well you did.
+
+Good luck!
--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -16,19 +16,31 @@
 /* var timerSeconds= 
 */
 
-var i=180;
+var i=90;
 
 function gameTimer(){
 	setInterval(function(){
-		document.getElementById("clock").innerHTML=(i + "s");
+		document.getElementById("clock").innerHTML=(i);
 		if(i>=1){
 			i--;
+
 		}
-		},1000)
-} 
+		},1000);
+		}		
 gameTimer();
 
-//action when click answers
+setTimeout(timeUp,1000 *92);
+
+function timeUp(){
+		alert("Time's Up!");	
+		alert("Great job! You answered " + right + " questions correctly.");	
+
+}
+
+
+
+
+//keep score when click answers
 
 var wrong=0;
 var right=0;
@@ -53,9 +65,11 @@ console.log(right); */
 //onload of results.html, display total right and wrong answers
 
 function displayScore(){
-	$("#rightTotal").html( right + " answered correctly! Way to go!");
-	$("#wrongTotal").html( wrong + " answered incorrectly. Nice try!");	
+	alert("Great job! You answered " + right + " questions correctly.");	
 }
+
+
+
 
 
 
